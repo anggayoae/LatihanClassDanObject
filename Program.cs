242a53b7880bf553e33efd69f7136439c2111dbc
anggayoae <127918323+anggayoae@users.noreply.github.com>
2023-03-30 20:43:42 +0700

    LatihanClassDanObject

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            saya.Nim = "123";
+            saya.Nama = "angga";
+            saya.Ipk = 4.15f;
+
+            kamu.Nim = "124";
+            kamu.Nama = "Jono";
+            kamu.Ipk = 4.12f;
+
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
